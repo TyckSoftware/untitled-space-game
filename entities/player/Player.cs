de5f740c2edf2def0fba8183f3cc7a79f9da8d0a
ThowV <thomasvisscher@outlook.com>
2022-10-24 21:58:52 +0200

    Implement character movement

--- entities/player/Player.cs
+++ entities/player/Player.cs
@@ -1,39 +1,36 @@
 using Godot;
-using System;
 
+/// <summary>
+/// Class containing methods and properties regarding
+/// movement and rotation of the player.
+/// </summary>
 public partial class Player : CharacterBody3D
 {
-    public const float Speed = 5.0f;
-    public const float JumpVelocity = 4.5f;
+    /// <summary>
+    /// The maximum velocity that the player
+    /// can reach.
+    /// </summary>
+    public const float MAX_VELOCITY = 5.0f;
 
-    // Get the gravity from the project settings to be synced with RigidBody nodes.
-    public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
-
+    /// <inheritdoc />
     public override void _PhysicsProcess(double delta)
     {
         Vector3 velocity = Velocity;
 
-        // Add the gravity.
-        if (!IsOnFloor())
-            velocity.y -= gravity * (float)delta;
-
-        // Handle Jump.
-        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-            velocity.y = JumpVelocity;
-
-        // Get the input direction and handle the movement/deceleration.
-        // As good practice, you should replace UI actions with custom gameplay actions.
+        // Get the input direction.
         Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
         Vector3 direction = (Transform.basis * new Vector3(inputDir.x, 0, inputDir.y)).Normalized();
+
+        // Apply movement forces for accelerating and decelerating.
         if (direction != Vector3.Zero)
         {
-            velocity.x = direction.x * Speed;
-            velocity.z = direction.z * Speed;
+            velocity.x = direction.x * MAX_VELOCITY;
+            velocity.z = direction.z * MAX_VELOCITY;
         }
         else
         {
-            velocity.x = Mathf.MoveToward(Velocity.x, 0, Speed);
-            velocity.z = Mathf.MoveToward(Velocity.z, 0, Speed);
+            velocity.x = Mathf.MoveToward(Velocity.x, 0, MAX_VELOCITY);
+            velocity.z = Mathf.MoveToward(Velocity.z, 0, MAX_VELOCITY);
         }
 
         Velocity = velocity;

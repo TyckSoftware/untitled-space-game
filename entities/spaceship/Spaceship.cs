da4563ec53b50fdbbed7226d3b24849baa327d52
ThowV <thomasvisscher@outlook.com>
2022-10-24 20:45:46 +0200

    Make ship RigidBody3d node and update movement code

--- entities/spaceship/Spaceship.cs
+++ entities/spaceship/Spaceship.cs
@@ -4,79 +4,91 @@ using Godot;
 /// Class containing methods and properties regarding
 /// movement and rotation of a spaceship.
 /// </summary>
-public partial class Spaceship : CharacterBody3D
+public partial class Spaceship : RigidBody3D
 {
     /// <summary>
     /// The maximum velocity that the spaceship
     /// can reach.
     /// </summary>
-    public const float MAX_VELOCITY = 50f;
+    public const float MAX_VELOCITY = 20f;
 
     /// <summary>
     /// The force that the thruster applies.
     /// </summary>
-    public const float THRUSTER_FORCE = .5f;
+    public const float THRUSTER_FORCE = 20f;
 
-    /// <summary>
-    /// The direction that the spaceship is moving towards.
-    /// </summary>
-    private float _direction = 0f;
+    public override void _IntegrateForces(PhysicsDirectBodyState3D state)
+    {
+        base._IntegrateForces(state);
+
+        ApplyVelocityClamp();
+
+        /*
+         * THIS LINE WOULD KIND OF FIX THE CONTINUOUS COLLISION DETECTION BUG:
+         * https://github.com/godotengine/godot/issues/67817#issuecomment-1288172328
+         */
+        if (state.GetContactCount() >= 1)
+        {
+            //LinearVelocity = Vector3.Zero;
+            //ApplyCentralImpulse(state.GetContactLocalNormal(0) * 3);
+        }
+    }
 
     /// <inheritdoc />
     public override void _PhysicsProcess(double delta)
     {
-        DoRotate();
-        DoMove((float)delta);
-        DoCameraFollow();
+        base._PhysicsProcess(delta);
+
+        ApplyRotationTorque();
+        ApplyMovementForce();
+        ApplyCameraPosition();
     }
 
     /// <summary>
-    /// Rotate the spaceship
+    /// Rotate the spaceship by applying a torque.
     /// </summary>
-    private void DoRotate()
+    private void ApplyRotationTorque()
     {
-        float input = Input.GetAxis("turn_left", "turn_right");
-        _direction = Mathf.Lerp(_direction, -input, .02f);
-        RotateY(Mathf.DegToRad(_direction));
+        float input = Input.GetAxis("turn_right", "turn_left");
+        ApplyTorque(Vector3.Up * input);
     }
 
     /// <summary>
-    /// Move the spaceship.
+    /// Move the spaceship by applying a force.
     /// </summary>
-    /// <param name="delta">
-    /// The delta time.
-    /// </param>
-    private void DoMove(float delta)
+    private void ApplyMovementForce()
     {
+        // Apply movement force.
         Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
-        Vector3 direction = (Transform.basis * new Vector3(inputDir.x, 0, inputDir.y)).Normalized();
+        Vector3 forceDirection = (Transform.basis * new Vector3(inputDir.x, 0, inputDir.y)).Normalized();
 
-        // Apply input force.
-        if (inputDir != Vector2.Zero)
+        ApplyCentralForce(forceDirection * THRUSTER_FORCE);
+
+        // Apply braking force.
+        float inputBrake = Input.GetActionStrength("brake");
+
+        if (inputBrake != 0)
         {
-            Velocity = Velocity.MoveToward(direction * MAX_VELOCITY, THRUSTER_FORCE);
-        }
-
-        // Apply breaking force.
-        if (Input.IsKeyPressed(Key.Space))
-        {
-            Velocity = Velocity.MoveToward(Vector3.Zero, THRUSTER_FORCE);
-        }
-
-        // Move and detect collisions.
-        KinematicCollision3D collisionInfo = MoveAndCollide(Velocity * delta);
-
-        // Apply bounce and friction when colliding with something.
-        if (collisionInfo != null)
-        {
-            Velocity = Velocity.Bounce(collisionInfo.GetNormal()) / 4;
+            ApplyCentralForce(-LinearVelocity.Normalized() * inputBrake * THRUSTER_FORCE);
         }
     }
 
     /// <summary>
-    /// Move the camera to follow the spaceship.
+    /// Clamp the velocity of the spaceship.
     /// </summary>
-    private void DoCameraFollow()
+    private void ApplyVelocityClamp()
+    {
+        if (LinearVelocity.Length() > MAX_VELOCITY)
+        {
+            LinearVelocity = LinearVelocity.Normalized() * MAX_VELOCITY;
+        }
+    }
+
+    /// <summary>
+    /// Move the camera to follow the spaceship
+    /// by setting the position of the parent rig.
+    /// </summary>
+    private void ApplyCameraPosition()
     {
         var cameraRig = GetViewport().GetCamera3d().GetParent<Node3D>();
         cameraRig.GlobalPosition = Transform.origin;

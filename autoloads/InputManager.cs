0583cfbab1c694e2f016a4e5bd97aa699b517471
ThowV <thomasvisscher@outlook.com>
2022-10-29 14:30:29 +0200

    Implement input device change event

--- autoloads/InputManager.cs
+++ autoloads/InputManager.cs
@@ -1,4 +1,6 @@
 using Godot;
+using System;
+using System.Linq;
 
 /// <summary>
 /// Holds various input related methods and properties.
@@ -15,20 +17,55 @@ public partial class InputManager : Node
     public InputEvent? LatestInputEvent { get; private set; }
 
     /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="inputEvent"></param>
+    [Signal]
+    public delegate void InputDeviceChangedEventHandler(InputEvent inputEvent);
+
+    private Type[] KeyboardInputEventTypes { get; set; } = new[]
+    {
+        typeof(InputEventKey),
+        typeof(InputEventMouseButton),
+        typeof(InputEventMouseMotion),
+    };
+
+    private Type[] ControllerInputEventTypes { get; set; } = new[]
+    {
+        typeof(InputEventJoypadButton),
+        typeof(InputEventJoypadMotion),
+    };
+
+    /// <summary>
     /// Event that is fired by Godot whenever
     /// any key is pressed.
     /// </summary>
     /// <param name="inputEvent"></param>
     public void _input(InputEvent inputEvent)
     {
-        LatestInputEvent = inputEvent;
-        // var t = inputEvent switch
-        // {
-        //     InputEventKey e => 1,
-        //     InputEventMouseButton mouseEvent => 2,
-        //     _ => 3
-        // };
+        Type inputEventType = inputEvent.GetType();
+        Type? latestInputEventType = LatestInputEvent?.GetType();
 
-        // GD.Print(t);
+        LatestInputEvent = inputEvent;
+
+        if (
+            latestInputEventType == null
+            /*
+             * Check if the input event is a keyboard event
+             * but the latest input event is not.
+             */
+            || KeyboardInputEventTypes.Contains(inputEventType)
+            && !KeyboardInputEventTypes.Contains(latestInputEventType)
+            /*
+             * Check if the input event is a controller event
+             * but the latest input event is not.
+             */
+            || ControllerInputEventTypes.Contains(inputEventType)
+            && !ControllerInputEventTypes.Contains(latestInputEventType)
+            )
+        {
+            GD.Print("(InputManager) Input device changed.");
+            EmitSignal(SignalName.InputDeviceChanged, inputEvent);
+        }
     }
 }
--- placeables/interactable_area/InteractableArea.cs
+++ placeables/interactable_area/InteractableArea.cs
@@ -2,21 +2,30 @@ using Godot;
 using System.Collections.Generic;
 using System.Linq;
 
+/// <summary>
+/// An area that can hold various interactions
+/// that will be displayed in the UI upon entering.
+/// </summary>
 public partial class InteractableArea : Area3D
 {
     /// <summary>
-    /// 
+    /// The UI container that the interaction hints
+    /// are drawn into.
     /// </summary>
     [Export]
-    public Container InteractableUIContainer { get; set; }
+    public Container InteractionHintsUIContainer { get; set; }
         = default!;
 
+    /// <summary>
+    /// The interactions that are registered in
+    /// this area.
+    /// </summary>
     [Export]
     public InteractableAreaInteraction[] Interactions { get; set; }
         = new InteractableAreaInteraction[0];
 
     /// <summary>
-    /// 
+    /// The interaction hints that are currently being displayed.
     /// </summary>
     private IList<Label> Labels { get; set; } = new List<Label>();
 
@@ -26,9 +35,16 @@ public partial class InteractableArea : Area3D
     /// </summary>
     private InputManager InputManager { get; set; } = default!;
 
+    /// <summary>
+    /// 
+    /// </summary>
+    private bool IsPlayerInArea { get; set; }
+
     public override void _Ready()
     {
         InputManager = GetNode<InputManager>("/root/InputManager");
+        InputManager.InputDeviceChanged +=
+            inputEvent => OnInputDeviceChanged(inputEvent);
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -43,47 +59,31 @@ public partial class InteractableArea : Area3D
         }
     }
 
+    /// <summary>
+    /// Event that is fired by Godot whenever
+    /// any key is pressed.
+    /// </summary>
+    /// <param name="inputEvent"></param>
+    public void _input(InputEvent inputEvent)
+    {
+        // GD.Print("input");
+    }
+
+    private void OnInputDeviceChanged(InputEvent inputEvent)
+    {
+        if (IsPlayerInArea)
+        {
+            ClearInteractionHints();
+            DrawInteractionHints();
+        }
+    }
+
     public void OnInteractableAreaBodyEntered(Node3D body)
     {
         if (body is Player)
         {
-            // Add the interactions as labels to the UI container.
-            Player player = (Player)body;
-
-            foreach (InteractableAreaInteraction interaction in Interactions)
-            {
-                // Get the input events for the interaction.
-                Godot.Collections.Array<InputEvent> inputEvents = InputMap.ActionGetEvents(interaction.KeyAction);
-
-                /*
-                 * Detmine the input event whose name is going to be displayed.
-                 * This is done by checking what input device is used.
-                 */
-                InputEvent? inputEvent = null;
-
-                switch (InputManager.LatestInputEvent)
-                {
-                    // Joypad
-                    case InputEventJoypadButton:
-                    case InputEventJoypadMotion:
-                        inputEvent = inputEvents.FirstOrDefault(ie => ie is InputEventJoypadButton or InputEventJoypadMotion);
-                        break;
-                    // Keyboard + Mouse
-                    case InputEventKey:
-                    case InputEventMouse:
-                        inputEvent = inputEvents.FirstOrDefault(ie => ie is InputEventKey or InputEventMouseButton);
-                        break;
-                }
-
-                string keyName = inputEvent != null ? inputEvent.AsText() : "UNBOUND";
-
-                // Create a label with the interaction hint.
-                Label label = new Label();
-                label.Text = $"Press {keyName} to {interaction.ActionDisplayText}";
-                Labels.Add(label);
-
-                InteractableUIContainer.AddChild(label);
-            }
+            IsPlayerInArea = true;
+            DrawInteractionHints();
         }
     }
 
@@ -91,12 +91,74 @@ public partial class InteractableArea : Area3D
     {
         if (body is Player)
         {
-            foreach (Label label in Labels)
-            {
-                InteractableUIContainer.RemoveChild(label);
-            }
-
-            Labels.Clear();
+            IsPlayerInArea = false;
+            ClearInteractionHints();
         }
+    }
+
+    /// <summary>
+    /// Draws the interaction hint for each interaction.
+    /// </summary>
+    private void DrawInteractionHints()
+    {
+        foreach (InteractableAreaInteraction interaction in Interactions)
+        {
+            DrawInteractionHint(interaction);
+        }
+    }
+
+    /// <summary>
+    /// Draws the interaction hint for the given interaction.
+    /// </summary>
+    /// <param name="interactableAreaInteraction">
+    /// The interaction to draw the hint for.
+    /// </param>
+    private void DrawInteractionHint(InteractableAreaInteraction interactableAreaInteraction)
+    {
+        // Get the input events for the interaction.
+        Godot.Collections.Array<InputEvent> inputEvents =
+            InputMap.ActionGetEvents(interactableAreaInteraction.KeyAction);
+
+        /*
+		 * Detmine the input event whose name is going to be displayed.
+		 * This is done by checking what input device is used.
+		 */
+        InputEvent? inputEvent = null;
+
+        switch (InputManager.LatestInputEvent)
+        {
+            // Joypad
+            case InputEventJoypadButton:
+            case InputEventJoypadMotion:
+                inputEvent = inputEvents.FirstOrDefault(ie => ie is InputEventJoypadButton or InputEventJoypadMotion);
+                break;
+            // Keyboard + Mouse
+            case InputEventKey:
+            case InputEventMouse:
+                inputEvent = inputEvents.FirstOrDefault(ie => ie is InputEventKey or InputEventMouseButton);
+                break;
+        }
+
+        string keyName = inputEvent != null ? inputEvent.AsText() : "UNBOUND";
+
+        // Create a label with the interaction hint.
+        Label label = new Label();
+        label.Text = $"Press {keyName} to {interactableAreaInteraction.ActionDisplayText}";
+        Labels.Add(label);
+
+        InteractionHintsUIContainer.AddChild(label);
+    }
+
+    /// <summary>
+    /// Clears the interaction hint for each interaction.
+    /// </summary>
+    private void ClearInteractionHints()
+    {
+        foreach (Label label in Labels)
+        {
+            InteractionHintsUIContainer.RemoveChild(label);
+        }
+
+        Labels.Clear();
     }
 }
